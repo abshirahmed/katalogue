adbaa926280a27236db544e28c392d7316f2d6a8
Abshir Ahmed <abshir.ahmed@cazoo.co.uk>
2022-05-16 07:48:10 +0100

    :zap: replace ts-jest with @swc/jest
    - remove gitmoji commit hook

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
-  // https://kulshekhar.github.io/ts-jest/docs/getting-started/presets
-  preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js', 'jest-extended/all'],
-  collectCoverageFrom: ['<rootDir>/src/**/*']
+  collectCoverageFrom: ['<rootDir>/src/**/*'],
+  transform: {
+    '^.+\\.(t|j)sx?$': ['@swc/jest']
+  }
 };
